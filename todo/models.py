15b5f5545298b7a5223bee4532517e9414603316
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-01 18:02:07 +0000

    added a template file

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
-# To override the string method of django in admin panel, for name attribute.
+# To override the string method of django in admin panel, for item name.
+
+
     def __str__(self):
         return self.name
+        
