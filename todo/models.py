fd83f80bdfee5c3a19e4d96522409943e2ba481e
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-01 17:44:28 +0000

    installed 0001_initial.py, override string method to display item names

--- todo/models.py
+++ todo/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+
+# To override the string method of django in admin panel, for name attribute.
+    def __str__(self):
+        return self.name
