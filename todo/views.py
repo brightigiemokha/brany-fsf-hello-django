77a4658333452363480823f2c0775c5ad878d1f2
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-05 10:40:50 +0000

    import forms.py to view.py

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 
@@ -19,4 +20,8 @@ def add_item(request):
         Item.objects.create(name=name, done=done)
 
         return redirect('get_todo_list')
-    return render(request, 'todo/add_item.html')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
