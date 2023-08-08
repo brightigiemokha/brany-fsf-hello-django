43ab54b232c8eb038a20ef70fdd0fa835d07a7dc
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-08 18:34:29 +0000

    add test_views for todo_list, add_item

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,1 +1,14 @@
 from django.test import TestCase
+
+
+class TestViews(TestCase):
+
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+    
+    def test_get_add_item_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/add_item.html')
